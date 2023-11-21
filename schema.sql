62e044373510a8cce1826ab446fa0e22db8407bb
Chiara Fiorentini <chiara.fvw@gmail.com>
2023-11-21 13:25:39 +0100

    add schema and seed data

---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+  username text PRIMARY KEY,
+  password text NOT NULL
+);
+
+CREATE TABLE categories (
+  id serial PRIMARY KEY,
+  title text UNIQUE NOT NULL,
+  username text NOT NULL
+);
+
+CREATE TABLE recipes (
+  id serial PRIMARY KEY,
+  title text UNIQUE NOT NULL,
+  prep_time integer NOT NULL,
+  serves integer NOT NULL,
+  ingredients text NOT NULL,
+  steps text NOT NULL,
+  username text NOT NULL,
+  category_id integer
+    NOT NULL
+    REFERENCES categories(id)
+    ON DELETE CASCADE
+);
