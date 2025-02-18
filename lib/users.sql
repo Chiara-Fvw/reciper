69164d25bca8f93b842e1b38e3a934dc9505ae82
chiara <chiara.fvw@gmail.com>
2025-02-18 11:02:51 +0100

    Add files

---
+++ lib/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, password)
+VALUES ('admin', '$2b$10$qYWDRmLlpgN5lGD1XeV9weWe1ECxRsVYg2MV/zW2gzcJ0jWnWzgVa'),
+       ('developer', '$2b$10$ZxS5GhYzR3VJjFSdVOlzDugp6NyQlYUFct/CAuapyx/Os4RNBrU9m');
